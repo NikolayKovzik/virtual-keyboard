9c57533c367ffc4b7c29ca8006c46764c9c75e71
Nikolay <nikolay.kovzik@gmail.com>
2022-05-10 00:13:03 +0300

    feat: keys array is added

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 /* eslint-disable*/
+import { keys } from "./keys.js";
 
-import { Keyboard } from "./Keyboard";
+
+import { Keyboard } from "./Keyboard.js";
 
 let keyboard = new Keyboard();
+
+console.log(keys)
+console.log("adsada")
